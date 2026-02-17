a6cb2edb75294b275a973c4b030981ee88c15e7c
Florenzaclinic <marketingflorenza@gmail.com>
2026-02-17 13:40:38 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx'; // 
+import App from './App.jsx'; 
 
-// สร้าง Root เพื่อเชื่อมต่อกับ <div id="root"> ในไฟล์ index.html
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// สั่งให้ React เริ่มวาดหน้าจอ
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-
